7e20e7a694d2e5957c67d35524305bb5f5b822cf
ahnyunki <ahnyunki00@gmail.com>
2024-09-01 14:28:27 +0900

    sse 예제

---
+++ src/main/java/com/sse/service/SseClientService.java
@@ -0,0 +1,35 @@
+package com.sse.service;
+
+import java.time.LocalTime;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+@Service
+@RequiredArgsConstructor
+public class SseClientService {
+
+    private static final Logger logger = LoggerFactory.getLogger(SseClientService.class);
+
+    public void consumeServerSentEvent() {
+        WebClient client = WebClient.create("http://localhost:8080/sse-server");
+        ParameterizedTypeReference<ServerSentEvent<String>> type
+                = new ParameterizedTypeReference<ServerSentEvent<String>>() {};
+
+        Flux<ServerSentEvent<String>> eventStream = client.get()
+                .uri("/stream-sse")
+                .retrieve()
+                .bodyToFlux(type);
+
+        eventStream.subscribe(
+                content -> logger.info("Time: {} - event: name[{}], id [{}], content[{}] ",
+                        LocalTime.now(), content.event(), content.id(), content.data()),
+                error -> logger.error("Error receiving SSE: {}", error),
+                () -> logger.info("Completed!!!"));
+    }
+}
